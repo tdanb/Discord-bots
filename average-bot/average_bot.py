52bc53300e13ada7313973d7af86384f7647449f
tt1803 <98007308+tt1803@users.noreply.github.com>
2022-02-05 15:10:43 -0500

    trying image manipulation

--- average-bot/average_bot.py
+++ average-bot/average_bot.py
@@ -9,13 +9,14 @@ import discord
 import time
 import asyncio
 from discord.ext import commands
-#imports to create member report
-import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib import style
-style.use("fivethirtyeight")
+import random
+
+#imports for image manipulation
+from PIL import Image
+from io import BytesIO
 
 client = discord.Client()
+client = commands.Bot(command_prefix = "")
 
 @client.event 
 # establishing user at login
@@ -62,4 +63,37 @@ async def on_message(message):
         file = discord.File("average-bot/images/cute.gif", filename="average-bot/images/cute.gif")
         await message.channel.send("", file=file)
 
-client.run("[token]")
+    # display a meme
+    elif "meme" in message.content.lower():
+        file = discord.File("average-bot/images/meme.png", filename="average-bot/images/meme.png")
+        await message.channel.send("lol 😂", file=file)
+
+    elif "j" in message.content.lower():
+        user = discord.Member
+        wanted = Image.open("average-bot/images/wanted.png")
+        asset = user.avatar_url_as(size = 128)
+        data = BytesIO(await asset.read())
+        pfp = Image.open(data)
+        pfp = pfp.resize((177,177))
+        wanted.past(pfp,(120,212))
+        wanted.save()
+        await message.channel.send(file = discord.File("average-bot/images/wanted.png"))
+
+@client.command()
+async def wanted(ctx, user: discord.Member = None):
+    if (user == None):
+        user = ctx.author
+
+    wanted = Image.open("average-bot/images/wanted.png")
+    asset = user.avatar_url_as(size = 128)
+    data = BytesIO(await asset.read())
+    pfp = Image.open(data)
+
+    pfp = pfp.resize((177,177))
+    wanted.past(pfp,(120,212))
+
+    wanted.save("average-bot/images/profile.png")
+
+    await ctx.send(file = discord.File("average-bot/images/profile.png"))
+
+client.run("OTM3OTg1OTQ2Njk4MjY0NTk2.YfjtfA.iRuZ7pKIRAx_tFqgAZGMe6GtAqs")
